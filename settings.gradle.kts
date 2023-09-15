7037f0445a95cd05f27b30ea8ba4134e16951e66
Laurie Anne Laberge <laberge.la@gmail.com>
2023-09-15 19:03:22 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "COEN390_AS1"
+include(":app")
+ 
