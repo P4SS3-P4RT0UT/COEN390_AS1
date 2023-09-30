cf9173f7e8798a63b7e76021e46d6ad5c4382148
Laurie Anne Laberge <laberge.la@gmail.com>
2023-09-30 14:21:43 -0400

    Set up settings activity toolbar

--- app/src/main/java/com/example/coen390_as1/SettingsActivity.java
+++ app/src/main/java/com/example/coen390_as1/SettingsActivity.java
@@ -1,10 +1,14 @@
 package com.example.coen390_as1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +19,7 @@ public class SettingsActivity extends AppCompatActivity {
 
     protected SharedPreferencesHelper sharedPreferencesHelper;
 
+    protected Toolbar toolbar;
     protected EditText counter1Name;
     protected EditText counter2Name;
     protected EditText counter3Name;
@@ -30,12 +35,38 @@ public class SettingsActivity extends AppCompatActivity {
         sharedPreferencesHelper = new SharedPreferencesHelper(SettingsActivity.this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_edit) {
+            counter1Name.setEnabled(true);
+            counter2Name.setEnabled(true);
+            counter3Name.setEnabled(true);
+            maxCounts.setEnabled(true);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     protected void setupUI() {
+
+        toolbar = findViewById(R.id.settings_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         counter1Name = findViewById(R.id.counter1edittext);
         counter2Name = findViewById(R.id.counter2edittext);
         counter3Name = findViewById(R.id.counter3edittext);
         maxCounts = findViewById(R.id.maxcountedittxt);
+
+        counter1Name.setEnabled(false);
+        counter2Name.setEnabled(false);
+        counter3Name.setEnabled(false);
+        maxCounts.setEnabled(false);
 
         saveBtn = findViewById(R.id.save_btn);
         saveBtn.setOnClickListener(onClickSaveBtn);
