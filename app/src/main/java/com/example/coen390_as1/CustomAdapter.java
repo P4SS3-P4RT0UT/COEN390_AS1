6472671db4e4e68a594ac75082fd8a9b09e154e1
Laurie Anne Laberge <laberge.la@gmail.com>
2023-10-08 15:10:13 -0400

    commented recyclerView

--- app/src/main/java/com/example/coen390_as1/CustomAdapter.java
+++ app/src/main/java/com/example/coen390_as1/CustomAdapter.java
@@ -11,7 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
 
-    // Extract list
+    // Extract list of counter keys
     private String[] eventList;
 
     /*
@@ -21,38 +21,39 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         this.eventList = eventList;
     }
 
+    /*
+    Create a new view
+    */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        // Create a new view
-        // Use predefined item layout
         View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.item_event, viewGroup, false);
-
         return new ViewHolder(view);
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
+    /*
+    Replace the view content
+    */
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        // Get a string from the list at some position and replace contents of the view with that string
         String eventName = eventList[position];
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
         viewHolder.eventName.setText(eventName);
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
+    /*
+    Return the size of the array of events
+     */
     @Override
     public int getItemCount() {
         return eventList.length;
     }
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder)
+    /*
+    Reference to the view
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private TextView eventName;
-
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             eventName = itemView.findViewById(R.id.event_name);
